5cbb53d8ef6393b5b948e144a5a9d7bf66d62ec1
angel-chandro <angel_chandro@hotmail.es>
2023-09-06 10:28:21 +0800

    test for make_positions

--- tests/test_sky_sim.py
+++ tests/test_sky_sim.py
@@ -14,3 +14,12 @@ def test_get_radec():
     if not ((expected[0]==answer[0]) and (expected[1]==answer[1])):
         raise AssertionError(f"get_radec gives {answer} instead of {expected}")
     return
+
+def test_make_positions_correct_number_of_sources():
+    from mymodule import sky_sim
+
+    ras,dec = sky_sim.make_positions(100,0,100)
+
+    if len(ras)!=100 or len(ras)!=100:
+        raise AssertionError(f"make_positions {len(ras)} sources instead of 100")
+    return
