987aca337218b7507bb1f2dfb7b7655a45601927
angel-chandro <angel_chandro@hotmail.es>
2023-09-06 10:09:32 +0800

    create test for get_radec

--- tests/test_sky_sim.py
+++ tests/test_sky_sim.py
@@ -4,3 +4,13 @@ def test_module_import():
     except Exception as e:
         raise AssertionError("Failed to import mymodule")
     return
+
+def test_get_radec():
+    from mymodule import sky_sim
+
+    expected = (14.215420962967535, 41.26916666666666)
+    answer = sky_sim.get_radec()
+
+    if not ((expected[0]==answer[0]) and (expected[1]==answer[1])):
+        raise AssertionError(f"get_radec gives {answer} instead of {expected}")
+    return
