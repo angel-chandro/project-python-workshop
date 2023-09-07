4c73a3c68755064dfe4b3dc2b51b81b63e83f290
angel-chandro <angel_chandro@hotmail.es>
2023-09-07 09:28:49 +0800

    implement logger

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -4,6 +4,12 @@
 from math import cos,pi
 from random import uniform
 import argparse
+import logging
+
+# configure logging (in global scope)
+logging.basicConfig(format="%(asctime)s, %(name)s:%(levelname)s %(message)s", level=logging.DEBUG)
+log = logging.getLogger("mylogger") # get or create a logger
+
 
 NSRC = 1_000_000
 # from wikipedia
@@ -22,6 +28,8 @@ def get_radec():
     dec : float
         The DEC, in degrees for Andromeda
     """
+    log.debug('Entering get_radec')
+
     # from wikipedia
     andromeda_ra = '00:42:44.3'
     andromeda_dec = '41:16:09'
@@ -32,6 +40,7 @@ def get_radec():
     h, m, s = andromeda_ra.split(':')
     ra = 15*(int(h)+int(m)/60+float(s)/3600)
     ra = ra/cos(dec*pi/180)
+    log.debug('Exiting get_radec') 
     return ra,dec
 
 
@@ -52,6 +61,8 @@ def make_positions(ra, dec, nsrc=NSRC):
     ras, decs : list
         A list of ra and dec coordinates.
     """
+    log.debug('Entering make_positions')
+
     # make 1000 stars within 1 degree of Andromeda
     ras = []
     decs = []
@@ -60,6 +71,7 @@ def make_positions(ra, dec, nsrc=NSRC):
         decs.append(dec + uniform(-1,1))
     # apply a filter
     ras,decs = crop_to_circle(ras,decs,ra,dec,1)
+    log.debug(f'Exiting make_positions with {ra}, {dec}, {nsrc}') 
     return ras,decs
 
 def crop_to_circle(ras, decs, ref_ra, ref_dec, radius):
@@ -80,20 +92,25 @@ def crop_to_circle(ras, decs, ref_ra, ref_dec, radius):
     ras, decs : list
         A list of ra and dec coordinates that pass our filter.
     """
+    log.debug('Entering crop_to_circle')
+
     ra_out = []
     dec_out = []
     for i in range(len(ras)):
         if (ras[i]-ref_ra)**2 + (decs[i]-ref_dec)**2 < radius**2:
             ra_out.append(ras[i])
             dec_out.append(ras[i])
+    log.debug('Exiting crop_to_circle') 
     return ra_out, dec_out
 
 def save_positions(ras, decs, out='catalog.csv'):
+
     # now write these to a csv file for use by my other program
     with open(out,'w',encoding='utf8') as f:
         print("id,ra,dec",file=f)
         for i in range(len(ras)):
             print(f"{i:07d}, {ras[i]:12f}, {decs[i]:12f}",file=f)
+    log.info(f'Writing file {out}') 
 
 
 def skysim_parser():
@@ -105,6 +122,7 @@ def skysim_parser():
     parser : argparse.ArgumentParser
         The parser for skysim.
     """
+
     parser = argparse.ArgumentParser(prog='sky_sim', prefix_chars='-')
     parser.add_argument('--ra', dest = 'ra', type=float, default=None,
                         help="Central ra (degrees) for the simulation location")
@@ -125,7 +143,6 @@ def main():
         ra = options.ra
         dec = options.dec
 
-    ra, dec = get_radec()
     ras, decs = make_positions(ra, dec, NSRC)
     save_positions(ras, decs, options.out)
 
